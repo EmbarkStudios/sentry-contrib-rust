d030a858419e40908af75c1e225873bf0bf43ed4
Jake Shadle <jake.shadle@embark-studios.com>
2020-11-24 12:29:19 +0100

    Remove useless function

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,3 @@ mod error;
 
 pub use breakpad_integration::BreakpadIntegration;
 pub use error::Error;
-
-pub fn upload_minidumps(hub: &sentry_core::Hub) {
-    hub.with_integration(|integration: &BreakpadIntegration| {
-        integration.upload_minidumps(hub);
-    });
-}
