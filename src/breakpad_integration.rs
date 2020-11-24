8f5ab9472be518a163884544c6a0a86055dc5bd1
Jake Shadle <jake.shadle@embark-studios.com>
2020-11-24 12:29:35 +0100

    Add back debug printing

--- src/breakpad_integration.rs
+++ src/breakpad_integration.rs
@@ -1,4 +1,4 @@
-use sentry_core::{protocol, ClientOptions};
+use sentry_core::protocol;
 
 fn read_metadata_to_envelope(path: &std::path::Path, envelope: &mut protocol::Envelope) {
     if !path.exists() {
@@ -7,16 +7,16 @@ fn read_metadata_to_envelope(path: &std::path::Path, envelope: &mut protocol::En
 
     let contents = match std::fs::read_to_string(path) {
         Ok(contents) => {
-            // Immediately remove the file we don't try to do this again
-            let _ = std::fs::remove_file(path);
+            // Immediately remove the file so we don't try to do this again
+            //let _ = std::fs::remove_file(path);
             contents
         }
-        Err(_e) => {
-            // sentry_debug!(
-            //     "unable to read crash metadata from '{}': {}",
-            //     path.display(),
-            //     e
-            // );
+        Err(e) => {
+            eprintln!(
+                "unable to read crash metadata from '{}': {}",
+                path.display(),
+                e
+            );
             return;
         }
     };
@@ -29,8 +29,8 @@ fn read_metadata_to_envelope(path: &std::path::Path, envelope: &mut protocol::En
                 Ok(event) => {
                     envelope.add_item(protocol::EnvelopeItem::Event(event));
                 }
-                Err(_e) => {
-                    //sentry_debug!("unable to deserialize Event: {}", e);
+                Err(e) => {
+                    eprintln!("unable to deserialize Event: {}", e);
                 }
             };
         }
@@ -42,8 +42,8 @@ fn read_metadata_to_envelope(path: &std::path::Path, envelope: &mut protocol::En
                 Ok(sess) => {
                     envelope.add_item(protocol::EnvelopeItem::SessionUpdate(sess));
                 }
-                Err(_e) => {
-                    //sentry_debug!("unable to deserialize SessionUpdate: {}", e);
+                Err(e) => {
+                    eprintln!("unable to deserialize SessionUpdate: {}", e);
                 }
             };
         }
@@ -54,29 +54,26 @@ fn read_metadata_to_envelope(path: &std::path::Path, envelope: &mut protocol::En
 pub struct BreakpadIntegration {
     crash_handler: Option<breakpad_handler::BreakpadHandler>,
     crash_dir: std::path::PathBuf,
-}
-
-impl sentry_core::Integration for BreakpadIntegration {
-    fn name(&self) -> &'static str {
-        "breakpad"
-    }
-
-    fn setup(&self, _cfg: &mut ClientOptions) {}
+    hub: std::sync::Arc<sentry_core::Hub>,
 }
 
 impl BreakpadIntegration {
     /// Creates a new Breakpad Integration, note that only *one* can exist
     /// in the application at a time!
-    pub fn new<P: AsRef<std::path::Path>>(crash_dir: P) -> Result<Self, crate::Error> {
+    pub fn new<P: AsRef<std::path::Path>>(
+        crash_dir: P,
+        hub: std::sync::Arc<sentry_core::Hub>,
+    ) -> Result<Self, crate::Error> {
         use std::io::Write;
 
         // Ensure the directory exists, breakpad should do this when writing crashdumps
         // anyway, but then again, it's C++ code, so I have low trust
         std::fs::create_dir_all(&crash_dir)?;
 
+        let crash_hub = hub.clone();
         let crash_handler = breakpad_handler::BreakpadHandler::attach(
             &crash_dir,
-            Box::new(|mut minidump_path: std::path::PathBuf| {
+            Box::new(move |mut minidump_path: std::path::PathBuf| {
                 // Create an event for crash so that we can add all of the context
                 // we can to it, the important information like stack traces/threads
                 // modules/etc is contained in the minidump recorded by breakpad
@@ -96,9 +93,9 @@ impl BreakpadIntegration {
                 // that we can serialize them to disk so that we can add them
                 // into the same envelope as the actual minidump
                 {
-                    let hub = sentry_core::Hub::main();
-                    if let Some(client) = hub.client() {
-                        hub.configure_scope(|scope| {
+                    if let Some(client) = crash_hub.client() {
+                        eprintln!("FILLING EVENT!");
+                        crash_hub.configure_scope(|scope| {
                             let assembled = client.assemble_event(event, Some(scope));
                             eve = assembled.0;
                             sess_update = assembled.1;
@@ -110,32 +107,38 @@ impl BreakpadIntegration {
 
                 // Serialize the envelope then the session update to their own JSON line
                 if let Some(eve) = eve {
-                    if let Err(_e) = serde_json::to_writer(&mut meta_data, &eve) {
-                        //sentry_debug!("failed to serialize event to crash metadata: {}", e);
+                    if let Err(e) = serde_json::to_writer(&mut meta_data, &eve) {
+                        eprintln!("failed to serialize event to crash metadata: {}", e);
                     }
                 }
 
                 let _ = writeln!(&mut meta_data);
 
                 if let Some(su) = sess_update {
-                    if let Err(_e) = serde_json::to_writer(&mut meta_data, &su) {
-                        // sentry_debug!(
-                        //     "failed to serialize session update to crash metadata: {}",
-                        //     e
-                        // );
+                    if let Err(e) = serde_json::to_writer(&mut meta_data, &su) {
+                        eprintln!(
+                            "failed to serialize session update to crash metadata: {}",
+                            e
+                        );
                     }
                 }
 
                 let _ = writeln!(&mut meta_data);
                 minidump_path.set_extension("metadata");
 
-                if let Err(_e) = std::fs::write(&minidump_path, &meta_data) {
-                    // sentry_debug!(
-                    //     "failed to write sentry crash metadata to '{}': {}",
-                    //     minidump_path.display(),
-                    //     e
-                    // );
+                if let Err(e) = std::fs::write(&minidump_path, &meta_data) {
+                    eprintln!(
+                        "failed to write sentry crash metadata to '{}': {}",
+                        minidump_path.display(),
+                        e
+                    );
                 }
+
+                eprintln!(
+                    "wrote {} of metadata for crash to {}",
+                    meta_data.len(),
+                    minidump_path.display()
+                );
             }),
         )?;
 
@@ -144,27 +147,28 @@ impl BreakpadIntegration {
         Ok(Self {
             crash_dir,
             crash_handler: Some(crash_handler),
+            hub,
         })
     }
 
     /// Run this once you have initialized Sentry to upload any minidumps + metadata
     /// that may exist from an earlier run
-    pub fn upload_minidumps(&self, hub: &sentry_core::Hub) {
+    pub fn upload_minidumps(&self) {
         // Scan the directory the integration was initialized with to find any
         // envelopes that have been serialized to disk and send + delete them
         let rd = match std::fs::read_dir(&self.crash_dir) {
             Ok(rd) => rd,
-            Err(_e) => {
-                // sentry_debug!(
-                //     "Unable to read crash directory '{}': {}",
-                //     self.crash_dir.display(),
-                //     e
-                // );
+            Err(e) => {
+                eprintln!(
+                    "Unable to read crash directory '{}': {}",
+                    self.crash_dir.display(),
+                    e
+                );
                 return;
             }
         };
 
-        let client = hub.client();
+        let client = self.hub.client();
 
         let client = match client {
             Some(c) => c,
@@ -186,12 +190,12 @@ impl BreakpadIntegration {
             let mut envelope = protocol::Envelope::new();
 
             let minidump_contents = match std::fs::read(&minidump_path) {
-                Err(_e) => {
-                    // sentry_debug!(
-                    //     "unable to read minidump from '{}': {}",
-                    //     minidump_path.display(),
-                    //     e
-                    // );
+                Err(e) => {
+                    eprintln!(
+                        "unable to read minidump from '{}': {}",
+                        minidump_path.display(),
+                        e
+                    );
 
                     let _ = std::fs::remove_file(&minidump_path);
 
@@ -202,7 +206,12 @@ impl BreakpadIntegration {
 
                     continue;
                 }
-                Ok(minidump) => minidump,
+                Ok(minidump) => {
+                    // Remove the minidump so we don't process it again
+                    let _ = std::fs::remove_file(&minidump_path);
+                    eprintln!("LOADED MINIDUMP FROM {}", minidump_path.display());
+                    minidump
+                }
             };
 
             envelope.add_item(protocol::EnvelopeItem::Attachment(protocol::Attachment {
@@ -215,6 +224,26 @@ impl BreakpadIntegration {
 
             // We might be able to attach metadata to the event, but it's optional
             read_metadata_to_envelope(&minidump_path, &mut envelope);
+
+            // An event_id is required, so if we were unable to get one from the .metadata
+            // we just use the guid in the filename of the minidump
+            if envelope.uuid().is_none() {
+                let event = protocol::Event {
+                    event_id: minidump_path
+                        .file_stem()
+                        .and_then(|fname| {
+                            fname
+                                .to_str()
+                                .and_then(|fs| fs.parse::<sentry_core::types::Uuid>().ok())
+                        })
+                        .unwrap_or_else(|| sentry_core::types::Uuid::new_v4()),
+                    level: protocol::Level::Fatal,
+                    timestamp: sentry_core::types::Utc::now(),
+                    ..Default::default()
+                };
+
+                envelope.add_item(event);
+            }
 
             client.send_envelope(envelope);
         }
