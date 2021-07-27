95e508bf1ef024dfba022b1623fc0b0823ece3ac
Jake Shadle <jake.shadle@embark-studios.com>
2021-07-27 11:22:38 +0200

    Refactor code to avoid need for fork

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
-#[cfg_attr(not(feature = "debug-logs"), allow(unused_variables))]
+macro_rules! debug_print {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "debug-logs")]
+        {
+            eprintln!("[bp] {}", format_args!($($arg)*));
+        }
+        #[cfg(not(feature = "debug-logs"))]
+        {
+            let _ = format_args!($($arg)*);
+        }
+    }
+}
+
 mod breakpad_integration;
 mod error;
+mod shared;
+mod transport;
 
 pub use breakpad_integration::{BreakpadIntegration, InstallOptions};
 pub use error::Error;
+pub use transport::{BreakpadTransport, BreakpadTransportFactory, CrashSendStyle};
