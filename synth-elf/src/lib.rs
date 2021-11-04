f25703a9b2c839b7f67f42ebe991f2d6442ae1fa
Jake Shadle <jake.shadle@embark-studios.com>
2021-11-04 18:36:12 +0100

    Finish up using a forked test-assembler

--- synth-elf/src/lib.rs
+++ synth-elf/src/lib.rs
@@ -19,29 +19,30 @@ impl Default for StringTable {
 
 impl StringTable {
     pub fn with_endian(endian: Endian) -> Self {
-        let this = Self {
+        let mut this = Self {
             section: Section::with_endian(endian),
             strings: std::collections::HashMap::new(),
         };
 
-        this.add("").0
+        this.section.set_start_const(0);
+        this.add("");
+        this
     }
 
-    pub fn add(mut self, string: impl Into<String>) -> (Self, Label) {
+    pub fn add(&mut self, string: impl Into<String>) -> Label {
         let string = string.into();
-        if let Some(label) = self.strings.get(&string).cloned() {
-            return (self, label.clone());
+        if let Some(label) = self.strings.get(&string) {
+            return label.clone();
         }
 
         let here = self.section.here();
-        self.section = self
-            .section
+        self.section
             .append_bytes(string.as_bytes())
             // null terminator
             .append_bytes(&[0]);
 
         self.strings.insert(string, here.clone());
-        (self, here)
+        here
     }
 }
 
@@ -124,19 +125,26 @@ impl NumCast for u64 {
     }
 }
 
-fn append_num_with_size(section: Section, is_64_bits: bool, num: impl NumCast) -> Section {
-    if is_64_bits {
-        section.D64(num.to_u64())
-    } else {
-        section.D32(num.to_u32())
-    }
+trait WithSize {
+    fn append_word(&mut self, is_64_bits: bool, num: impl NumCast) -> &mut Self;
+    fn append_word_label(&mut self, is_64_bits: bool, label: &Label) -> &mut Self;
 }
 
-fn append_label_with_size(section: Section, is_64_bits: bool, label: &Label) -> Section {
-    if is_64_bits {
-        section.D64(label)
-    } else {
-        section.D32(label)
+impl WithSize for Section {
+    fn append_word(&mut self, is_64_bits: bool, num: impl NumCast) -> &mut Section {
+        if is_64_bits {
+            self.D64(num.to_u64())
+        } else {
+            self.D32(num.to_u32())
+        }
+    }
+
+    fn append_word_label(&mut self, is_64_bits: bool, label: &Label) -> &mut Section {
+        if is_64_bits {
+            self.D64(label)
+        } else {
+            self.D32(label)
+        }
     }
 }
 
@@ -168,7 +176,7 @@ impl Elf {
     pub fn new(machine: u16, file_class: ElfClass, endian: Endian) -> Self {
         let mut section = Section::with_endian(endian);
 
-        section = section
+        section
             .set_start_const(0)
             .append_bytes(header::ELFMAG)
             // ei_class
@@ -189,28 +197,27 @@ impl Elf {
 
         debug_assert_eq!(section.size() as usize, header::SIZEOF_IDENT);
 
-        section = section
-            // e_type
-            .D16(header::ET_EXEC)
-            // e_machine
-            .D16(machine)
-            // e_version
-            .D32(header::EV_CURRENT as u32);
-
         let program_header_label = Label::new();
         let section_header_label = Label::new();
         let program_count_label = Label::new();
         let section_count_label = Label::new();
         let section_header_string_index = Label::new();
 
-        // e_entry
-        section = append_num_with_size(section, file_class.is_64(), 0 as u32);
-        // e_phoff
-        section = append_label_with_size(section, file_class.is_64(), &program_header_label);
-        // e_shoff
-        section = append_label_with_size(section, file_class.is_64(), &section_header_label);
+        let is_64_bits = file_class.is_64();
 
-        section = section
+        section
+            // e_type
+            .D16(header::ET_EXEC)
+            // e_machine
+            .D16(machine)
+            // e_version
+            .D32(header::EV_CURRENT as u32)
+            // e_entry
+            .append_word(is_64_bits, 0u32)
+            // e_phoff
+            .append_word_label(is_64_bits, &program_header_label)
+            // e_shoff
+            .append_word_label(is_64_bits, &section_header_label)
             // e_flags
             .D32(0)
             // e_ehsize
@@ -218,15 +225,15 @@ impl Elf {
             // e_phentsize
             .D16(file_class.phentsize())
             // e_phnum
-            .D16(program_count_label)
+            .D16(&program_count_label)
             // e_shentsize
             .D16(file_class.shentsize())
             // e_shnum
-            .D16(section_count_label)
+            .D16(&section_count_label)
             // e_shstrndx
-            .D16(section_header_string_index);
+            .D16(&section_header_string_index);
 
-        let this = Self {
+        let mut this = Self {
             section,
             addr_size: file_class.addr_size(),
             program_header_label,
@@ -242,18 +249,13 @@ impl Elf {
         };
 
         // Empty section for SHN_UNDEF
-        this.add_section("", Section::new(), elf::section_header::SHT_NULL)
-            .0
+        this.add_section("", Section::new(), elf::section_header::SHT_NULL);
+        this
     }
 
     /// Add the section to the section header table and append it to the file.
     /// Returns the index of the section in the section header table.
-    pub fn add_section(
-        self,
-        name: impl Into<String>,
-        section: Section,
-        kind: u32,
-    ) -> (Self, usize) {
+    pub fn add_section(&mut self, name: impl Into<String>, section: Section, kind: u32) -> usize {
         self.add_section_with_attrs(name, section, kind, SectionAttrs::default())
     }
 
@@ -261,99 +263,159 @@ impl Elf {
     /// table and append it to the file. Returns the index of the section in the
     /// section header table.
     pub fn add_section_with_attrs(
-        mut self,
+        &mut self,
         name: impl Into<String>,
         section: Section,
         kind: u32,
         attrs: SectionAttrs,
-    ) -> (Self, usize) {
-        let (shs, string_label) = self.section_header_strings.add(name);
-
+    ) -> usize {
+        let string_label = self.section_header_strings.add(name);
         let size = section.size();
+        let is_64_bits = self.addr_size == 8;
 
-        let mut section_headers = self
-            .section_headers
+        let elf_section = Self::do_add_section(
+            &mut self.section_headers,
+            string_label,
+            size,
+            is_64_bits,
+            section,
+            kind,
+            attrs,
+        );
+
+        self.sections.push(elf_section);
+        self.sections.len() - 1
+    }
+
+    fn do_add_section(
+        section_headers: &mut Section,
+        string_label: Label,
+        size: u64,
+        is_64_bits: bool,
+        section: Section,
+        kind: u32,
+        attrs: SectionAttrs,
+    ) -> ElfSection {
+        let offset_label = Label::new();
+
+        section_headers
             // sh_name
             .D32(string_label)
             // sh_type
-            .D32(kind);
+            .D32(kind)
+            // sh_flags
+            .append_word(is_64_bits, attrs.flags)
+            // sh_addr
+            .append_word(is_64_bits, attrs.addr);
 
-        let is_64_bits = self.addr_size == 8;
-
-        let offset_label = Label::new();
-
-        // sh_flags
-        section_headers = append_num_with_size(section_headers, is_64_bits, attrs.flags);
-        // sh_addr
-        section_headers = append_num_with_size(section_headers, is_64_bits, attrs.addr);
-        // sh_offset
-        section_headers = append_label_with_size(section_headers, is_64_bits, &offset_label);
-        // sh_size
-        section_headers = append_num_with_size(section_headers, is_64_bits, size);
-        section_headers = section_headers
+        section_headers
+            // sh_offset
+            .append_word_label(is_64_bits, &offset_label)
+            // sh_size
+            .append_word(is_64_bits, size)
             // sh_link
             .D32(attrs.link)
             // sh_info
-            .D32(0);
+            .D32(0)
+            // sh_addralign
+            .append_word(is_64_bits, 0u32)
+            // sh_entsize
+            .append_word(is_64_bits, attrs.entsize);
 
-        // sh_addralign
-        section_headers = append_num_with_size(section_headers, is_64_bits, 0 as u32);
-        // sh_entsize
-        section_headers = append_num_with_size(section_headers, is_64_bits, attrs.entsize);
-
-        self.sections.push(ElfSection {
+        ElfSection {
             inner: section,
             kind,
             addr: attrs.addr as u32,
             offset: attrs.offset as u32,
             offset_label,
             size: size as u32,
-        });
-
-        let index = self.sections.len() - 1;
-
-        (
-            Self {
-                section: self.section,
-                addr_size: self.addr_size,
-                program_header_label: self.program_header_label,
-                program_count: self.program_count,
-                program_count_label: self.program_count_label,
-                program_headers: self.program_headers,
-                section_header_label: self.section_header_label,
-                section_count_label: self.section_count_label,
-                section_headers,
-                section_header_string_index: self.section_header_string_index,
-                section_header_strings: shs,
-                sections: self.sections,
-            },
-            index,
-        )
+        }
     }
 
-    pub fn add_segment(mut self, 
+    pub fn add_segment(&mut self, start: usize, end: usize, kind: u32, flags: u32) {
+        self.program_count += 1;
+        let is_64_bits = self.addr_size == 8;
+
+        // p_type
+        self.program_headers.D32(kind);
+
+        if is_64_bits {
+            // p_flags
+            self.program_headers.D32(flags);
+        }
+
+        let mut file_size = 0;
+        let mut mem_size = 0;
+        let mut prev_was_nobits = false;
+        for section in &self.sections[start..end] {
+            let mut size = section.size as u64;
+            if section.kind != elf::section_header::SHT_NOBITS {
+                debug_assert!(!prev_was_nobits);
+                size = (size + 3) & !3;
+                file_size += size;
+            } else {
+                prev_was_nobits = true;
+            }
+
+            mem_size += size;
+        }
+
+        let section = &self.sections[start];
+
+        self.program_headers
+            // p_offset
+            .append_word_label(is_64_bits, &section.offset_label)
+            // p_vaddr
+            .append_word(is_64_bits, section.addr)
+            // p_paddr
+            .append_word(is_64_bits, section.addr)
+            // p_filesz
+            .append_word(is_64_bits, file_size)
+            // p_memsz
+            .append_word(is_64_bits, mem_size);
+
+        if !is_64_bits {
+            // p_flags
+            self.program_headers.D32(flags);
+        }
+
+        // p_align
+        self.program_headers.append_word(is_64_bits, 0u32);
+    }
 
     /// Finalizes the elf
-    pub fn finish(self) -> Option<Vec<u8>> {
+    pub fn finish(mut self) -> Option<Vec<u8>> {
         self.section_header_string_index
             .set_const(self.sections.len() as u64);
 
-        let (mut this, _) = self.add_section(
-            ".shstrtab",
-            self.section_header_strings.section,
-            elf::section_header::SHT_STRTAB,
-        );
+        {
+            let string_label = self.section_header_strings.add(".shstrtab");
+            let size = self.section.size();
+            let is_64_bits = self.addr_size == 8;
 
-        if this.program_count > 0 {
-            this.section = this
-                .section
-                .mark(&this.program_header_label)
-                .append_section(this.program_headers);
-        } else {
-            this.program_header_label.set_const(0);
+            let elf_section = Self::do_add_section(
+                &mut self.section_headers,
+                string_label,
+                size,
+                is_64_bits,
+                self.section_header_strings.section,
+                elf::section_header::SHT_STRTAB,
+                SectionAttrs::default(),
+            );
+
+            self.sections.push(elf_section);
         }
 
-        for esec in this.sections {
+        if self.program_count > 0 {
+            self.section
+                .mark(&self.program_header_label)
+                .append_section(self.program_headers);
+        } else {
+            self.program_header_label.set_const(0);
+        }
+
+        let num_sections = self.sections.len() as u64;
+        for esec in self.sections {
             // NULL and NOBITS sections have no content, so they don't need to
             // be written to the file.
             if esec.kind == elf::section_header::SHT_NULL {
@@ -361,24 +423,20 @@ impl Elf {
             } else if esec.kind == elf::section_header::SHT_NOBITS {
                 esec.offset_label.set_const(esec.offset as u64);
             } else {
-                this.section = this
-                    .section
-                    .mark(&esec.offset_label)
-                    .append_section(esec.inner)
-                    .align(4);
+                self.section.mark(&esec.offset_label);
+
+                self.section.append_section(esec.inner).align(4);
             }
         }
 
-        this.section_count_label
-            .set_const(this.sections.len() as u64);
-        this.program_count_label
-            .set_const(this.program_count as u64);
+        self.section_count_label.set_const(num_sections);
+        self.program_count_label
+            .set_const(self.program_count as u64);
 
-        let finished = this
-            .section
-            .mark(&this.section_header_label)
-            .append_section(this.section_headers);
+        self.section
+            .mark(&self.section_header_label)
+            .append_section(self.section_headers);
 
-        finished.get_contents()
+        self.section.get_contents()
     }
 }
